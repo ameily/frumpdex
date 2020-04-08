17a37753ccae81c74e546377f26618d61e4d1ad0
Adam Meily <meily.adam@gmail.com>
2020-04-08 19:45:39 -0400

    add silent config param

--- frumpdex/config.py
+++ frumpdex/config.py
@@ -1,16 +1,21 @@
+#
+# Copyright (c) 2020, Adam Meily
+# All rights reserved.
+#
 from cincoconfig import *  # pylint: disable=unused-wildcard-import
 
 schema = Schema()
-schema.mode = ApplicationModeField(default='production')
+schema.mode = ApplicationModeField(default='production', modes=['debug', 'production'])
 
 schema.http.port = PortField(default=5000, required=True)
 schema.http.address = IPv4AddressField(default='127.0.0.1', required=True)
-schema.http.cors_allowed_origins = ListField(StringField())
+schema.http.cors.allowed_origins = ListField(StringField())
 
 schema.gitlab.private_token = StringField()
 schema.gitlab.url = UrlField()
 
 schema.logfile = FilenameField()
+schema.silent = BoolField(default=False)
 
 schema.mongodb.url = UrlField(default='mongodb://localhost:27017', required=True)
 
